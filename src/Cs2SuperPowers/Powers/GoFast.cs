92a9dcaa17516182124df630a8eb610ab00ac871
Daniel Jones <daniel@danieljon.es>
2025-10-03 23:08:16 +1000

    register correct events in GoFast

--- src/Cs2SuperPowers/Powers/GoFast.cs
+++ src/Cs2SuperPowers/Powers/GoFast.cs
@@ -19,10 +19,10 @@ public class GoFast(IPlayerHud playerHud) : BasePower(playerHud)
 
     protected override void OnInitialize()
     {
-        RegisterEventHandler<EventRoundFreezeEnd>(OnRoundFreezeEnd);
+        RegisterEventHandler<EventRoundStart>(OnRoundStart);
     }
 
-    private HookResult OnRoundFreezeEnd(EventRoundFreezeEnd @event, GameEventInfo _)
+    private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo _)
     {
         foreach (var player in PlayerPowers.Instance.GetPlayersWithPower(this))
         {
