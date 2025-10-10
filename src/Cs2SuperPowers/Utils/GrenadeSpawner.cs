9070fa662cb631fd3ae0865ea68e9c9bc677df63
Daniel Jones <daniel@danieljon.es>
2025-10-10 16:32:19 +1100

    fix some formatting stuff

--- src/Cs2SuperPowers/Utils/GrenadeSpawner.cs
+++ src/Cs2SuperPowers/Utils/GrenadeSpawner.cs
@@ -6,29 +6,13 @@ using CounterStrikeSharp.API.Modules.Utils;
 
 namespace Cs2SuperPowers.Utils;
 
-/// <summary>
-/// Shared utility class for spawning grenades across different powers.
-/// Contains the common memory function and basic grenade creation logic.
-/// </summary>
 public static class GrenadeSpawner
 {
-    /// <summary>
-    /// Memory function for creating CHE grenade projectiles.
-    /// This is the core function used by all grenade-spawning powers.
-    /// </summary>
     public static MemoryFunctionWithReturn<IntPtr, IntPtr, IntPtr, IntPtr, IntPtr, int, CHEGrenadeProjectile>
         CHEGrenadeProjectile_CreateFunc = new(
         "55 4C 89 C1 48 89 E5 41 57 49 89 FF 41 56 49 89 D6"
     );
 
-    /// <summary>
-    /// Creates a grenade at the specified position with the given angle and velocity.
-    /// This is the basic grenade creation function used by all powers.
-    /// </summary>
-    /// <param name="position">The spawn position of the grenade</param>
-    /// <param name="angle">The spawn angle of the grenade</param>
-    /// <param name="velocity">The initial velocity of the grenade</param>
-    /// <returns>The created grenade projectile, or null if creation failed</returns>
     public static CBaseCSGrenadeProjectile? CreateGrenade(CounterStrikeSharp.API.Modules.Utils.Vector position, CounterStrikeSharp.API.Modules.Utils.QAngle angle, CounterStrikeSharp.API.Modules.Utils.Vector velocity)
     {
         try
@@ -67,11 +51,7 @@ public static class GrenadeSpawner
 
                 if (owner.PlayerPawn?.Value != null)
                 {
-                     Server.NextFrame(() =>
-                    {
-                        Schema.SetSchemaValue(grenade.Handle, "CBaseEntity", "m_hOwnerEntity", owner.PlayerPawn.Value.Handle);
-                    });
-                    owner.PrintToChat($"[cs2sp/debug] {owner.PlayerName} spawned a grenade at decoy position.");
+                    //owner.PrintToChat($"[cs2sp/debug] {owner.PlayerName} spawned a grenade at decoy position.");
                     grenade.AcceptInput("InitializeSpawnFromWorld", owner.PlayerPawn.Value, owner.PlayerPawn.Value, "");
                 }
             }
