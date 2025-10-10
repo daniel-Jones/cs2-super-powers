e252d438336968c03c9010a5fbfdfc6f0106a04d
ScruffyRules <5356779+ScruffyRules@users.noreply.github.com>
2025-10-10 16:50:00 +1030

    Cleanup GrenandeSpawner

--- src/Cs2SuperPowers/Utils/GrenadeSpawner.cs
+++ src/Cs2SuperPowers/Utils/GrenadeSpawner.cs
@@ -1,6 +1,4 @@
-using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 using CounterStrikeSharp.API.Modules.Utils;
 
@@ -13,7 +11,7 @@ public static class GrenadeSpawner
         "55 4C 89 C1 48 89 E5 41 57 49 89 FF 41 56 49 89 D6"
     );
 
-    public static CBaseCSGrenadeProjectile? CreateGrenade(CounterStrikeSharp.API.Modules.Utils.Vector position, CounterStrikeSharp.API.Modules.Utils.QAngle angle, CounterStrikeSharp.API.Modules.Utils.Vector velocity, CCSPlayerPawn? owner = null)
+    public static CBaseCSGrenadeProjectile? CreateGrenade(Vector position, QAngle angle, Vector velocity, CCSPlayerPawn? owner = null)
     {
         try
         {
